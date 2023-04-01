7f3223aa7d4744e066b4dba1b347bc3830476062
Bouke Nijhuis <bouke.nijhuis@gmail.com>
2023-04-01 16:23:35 +0200

    added integration test

--- src/test/java/com/example/score2csv/Score2csvApplicationTests.java
+++ src/test/java/com/example/score2csv/Score2csvApplicationTests.java
@@ -1,13 +1,38 @@
 package com.example.score2csv;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class Score2csvApplicationTests {
+
+    @Autowired
+    MockMvc mockMvc;
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void helloTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello")).andDo(print())
+                .andExpect(content().string("Hello"));
+    }
+
+    @Test
+    void scoreTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/score").contentType(MediaType.APPLICATION_JSON).content("{\"datetime\": \"2023-04-01T11:08:24.923475\", \"username\": \"th,ijs\", \"email\": \"thij,s@abc.com\", \"score\": 7}\n")).andDo(print())
+              .andExpect(status().isOk()).andExpect(content().string("Input[datetime=2023-04-01T11:08:24.923475, username=thijs, email=thijs@abc.com, score=7]"));
     }
 
 }
