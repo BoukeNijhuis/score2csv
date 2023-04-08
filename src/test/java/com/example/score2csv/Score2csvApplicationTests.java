5e489d8bc073ba76075aa718cf39cc8e69a90f62
Bouke Nijhuis <bouke.nijhuis@gmail.com>
2023-04-08 18:49:05 +0200

    one should also fix the tests after modifying the code :facepalm

--- src/test/java/com/example/score2csv/Score2csvApplicationTests.java
+++ src/test/java/com/example/score2csv/Score2csvApplicationTests.java
@@ -1,12 +1,17 @@
 package com.example.score2csv;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.io.File;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -18,6 +23,12 @@ class Score2csvApplicationTests {
 
     @Autowired
     MockMvc mockMvc;
+
+    @BeforeEach
+    @AfterEach
+    void setup() {
+        new File("data.csv").delete();
+    }
 
     @Test
     void contextLoads() {
@@ -32,7 +43,7 @@ class Score2csvApplicationTests {
     @Test
     void scoreTest() throws Exception {
         String postContent = "{\"datetime\": \"2023-04-01T11:08:24.923475\", \"username\": \"th,ijs\", \"email\": \"thij,s@abc.com\", \"score\": 7}\n";
-        String expectedContent = "Input[datetime=2023-04-01T11:08:24.923475, username=thijs, email=thijs@abc.com, score=7]";
+        String expectedContent = "\"" + HttpStatus.CREATED.getReasonPhrase().toUpperCase() +"\"";
         mockMvc.perform(MockMvcRequestBuilders.post("/score").contentType(MediaType.APPLICATION_JSON).content(postContent)).andDo(print())
               .andExpect(status().isOk()).andExpect(content().string(expectedContent));
     }
